47de6688beb96f1646053678dba945e46bcd93e8
Lucas Barbula <lucas.barbula@gmail.com>
2016-08-16 08:57:27 -0600

    working local storage

--- www/js/controllers/gameRoom-controller.js
+++ www/js/controllers/gameRoom-controller.js
@@ -1,12 +1,15 @@
 angular
   .module('dictionaryGame')
-  .controller('gameRoom-controller', ['$scope','gameFactory','socketFactory', '$stateParams', function($scope, gameFactory,socketFactory, $stateParams){
+  .controller('gameRoom-controller', ['$scope','gameFactory','socketFactory', '$stateParams', '$localStorage', function($scope, gameFactory,socketFactory, $stateParams, $localStorage){
     $scope.players = []
     $scope.socket = socketFactory.connect
-    $scope.socket.on('player-joined', function(data){
-      $scope.$apply(function (){
-        $scope.players.push(data)
-        console.log($scope.players)
-      })
+    $scope.socket.on('player-joined', function(userData, gameData){
+      console.log('userData=', userData)
+      console.log('gameData=', gameData)
+      $localStorage.sdUserName = 'Hello World!'
+      // $scope.$apply(function (){
+      //   $scope.players.push(data)
+      //   console.log($scope.players)
+      // })
     })
   }])
