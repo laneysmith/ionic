de4cab075b46429103256dc64feb4d462266a23a
Laney Smith <lsmith.unc@gmail.com>
2016-08-16 11:53:58 -0600

    Idk

--- www/js/controllers/gameRoom-controller.js
+++ www/js/controllers/gameRoom-controller.js
@@ -1,15 +1,15 @@
 angular
   .module('dictionaryGame')
   .controller('gameRoom-controller', ['$scope','gameFactory','socketFactory', '$stateParams', '$localStorage', function($scope, gameFactory,socketFactory, $stateParams, $localStorage){
-    $scope.players = []
+    $scope.player = ''
+    $scope.gameId = $stateParams.gameId
+    $scope.gameData = {}
     $scope.socket = socketFactory.connect
     $scope.socket.on('player-joined', function(userData, gameData){
-      console.log('userData=', userData)
-      console.log('gameData=', gameData)
-      $localStorage.sdUserName = 'Hello World!'
-      // $scope.$apply(function (){
-      //   $scope.players.push(data)
-      //   console.log($scope.players)
-      // })
+      $scope.gameData = gameData
+      $localStorage.sdUserName = userData.username
+      $scope.$apply(function (){
+        $scope.player = $localStorage.sdUserName
+      })
     })
   }])
