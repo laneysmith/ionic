15c7f8d056cb3c49c4614a12ed5ef9a626541dbd
Laney Smith <lsmith.unc@gmail.com>
2016-08-16 17:55:01 -0600

    Get ready to add angular socket.io

--- www/js/controllers/gameRoom-controller.js
+++ www/js/controllers/gameRoom-controller.js
@@ -1,17 +1,27 @@
 angular
   .module('dictionaryGame')
-  .controller('gameRoom-controller', ['$scope','gameFactory','socketFactory', function($scope, gameFactory,socketFactory){
+  .controller('gameRoom-controller', ['$scope','gameFactory','socketFactory', 'btford.socket-io', function($scope, gameFactory,socketFactory){
     $scope.player = ''
+    $scope.role = ''
     $scope.gameId = 0
     $scope.gameData = {}
     $scope.socket = socketFactory.connect
 
-    $scope.socket.on('player-joined', function(userData, gameData){
-      console.log('userData=',userData);
-      $scope.gameData = gameData
+    $scope.socket.on('set-user-info', function(userSpecificData){
+      console.log('set-user-info data=', userSpecificData);
       $scope.$apply(function (){
-        $scope.gameId = userData.gameId
-        $scope.player = userData
+        $scope.player = userSpecificData.username
+        $scope.role = userSpecificData.role
+        $scope.gameId = userSpecificData.gameId
+        $scope.gameData = userSpecificData.gameInfo
       })
     })
+
+    $scope.socket.on('player-joined', function(gameData){
+      console.log('gameData=',gameData);
+      $scope.$apply(function (){
+        $scope.gameData = gameData
+      })
+    })
+
   }])
